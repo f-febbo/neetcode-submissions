85c1567ad688720befd3fe67219d53b8b62ad91d
Francesco Febbo <104390035+f-febbo@users.noreply.github.com>
2026-06-27 13:55:28 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (const char& c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                char open = (c == ')') ? '(' : (c == ']') ? '[' : '{';
+                if (st.empty() || st.top() != open) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
